5814052c2b23ab62d8721cb6f0c88af72c47f5dc
majohurt28 <113456623+majohurt28@users.noreply.github.com>
2023-08-18 03:48:24 -0500

    Fetch api ID

--- index.js
+++ index.js
@@ -1,20 +1,121 @@
 
 const Pokemons = document.getElementById('Pokemons');
+const PokemonsEvo = document.getElementById('PokemonsEvo');
 
 let UrlApi = 'https://pokeapi.co/api/v2/pokemon/';
 
+ let pokemonList = [];
+pokemonList.push(1);
+pokemonList.push(4);
+pokemonList.push(393);
+
+let pokemonListname = [];
 
 
-async function Getpokeapi(url){
-       let response = await fetch(url);
+
+let Apiid = null;
+let Evolution = null;
+
+
+async function Getpoke(id){
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+    let data1 = await response.json();
+    console.log(data1);
+
+    Pokemons.innerHTML += `
+    <div class="sheet-containercard">
+    <img class="sheet" src="${data1.sprites.front_default}">
+    <h1> ${data1.name}</h1>
+    <h2> ${data1.weight}Kg</h2>
+    </div>`;
+
+}
+
+async function Getevo(id){
+    let response = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}/`);
+    let data2 = await response.json();
+    console.log(data2);
+
+    PokemonsEvo.innerHTML += `
+    <div class="sheet-containercard">
+    <img class="sheet" src="">
+    <h1 id="titlepok"> </h1>
+    </div>`;
+}
+
+async function Getpokemons(){
+    for(let i=0 ; i<pokemonList.length; i++){
+   await Getpoke(pokemonList[i])
+    }}
+Getpokemons();
+
+async function Getevolutions(){
+    for(let i=0 ; i<pokemonList.length; i++){
+   await Getevo(pokemonList[i])
+    }}
+    Getevolutions();
+
+
+  /*   document.addEventListener('Change', function() {
+        var button = document.createElement('input');
+        button.type = 'button';
+        button.id = 'Evolution';
+        button.value = 'Submit';
+        button.className = 'btn';
+
+        button.onclick = function() {
+
+        };
+
+        var container = document.getElementById('container');
+        container.appendChild(button);
+    }, false);
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function card (){
+   let cardholder = document.getElementById("Pokemons")
+
+
+
+
+
+};
+ */
+
+
+
+/*****2pt */
+
+
+/*
+ async function Getpokeapi(id){
+       let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
        let data = await response.json();
        console.log(data);
 
        Pokemons.innerHTML += `<div id="Pokecard">
                 <img id="Pokeimg" src="${data.sprites.front_default}">
-                <h1> Name: ${data.name}</h1>
+                <h1> ${data.name}</h1>
             </div>`;
 }
+
+Getpokeapi( 1)
 
 
 let pokemonList = [];
@@ -28,10 +129,8 @@ async function Getpokemon (){
         console.log(url);
         await Getpokeapi(url);
     }
-}
+} */
 
 
-function card (){}
 
-Getpokemon();
 
